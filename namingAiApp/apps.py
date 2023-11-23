8313d1384168a821121f29c0da63296c5e3b2e72
clap_0 <clap-0@naver.com>
2023-11-24 02:05:19 +0900

    refactor: recommendationApp에서 namingAiApp으로 이름 변경

--- namingAiApp/apps.py
+++ namingAiApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RecommendationappConfig(AppConfig):
+class NamingAiappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recommendationApp'
+    name = 'namingAiApp'
