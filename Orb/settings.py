6427cd345785654189cbbfb2828a7f7f3429f1d3
nahowo <nahowo@naver.com>
2023-11-24 00:08:17 +0900

    create wordcloud app

--- Orb/settings.py
+++ Orb/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'vote',
     'rest_framework',
+    'wordcloud',
 ]
 
 MIDDLEWARE = [
--- Orb/urls.py
+++ Orb/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vote/', include('vote.urls')),
+    # path('wordcloud/',include('wordcloud.urls')),
 ]
