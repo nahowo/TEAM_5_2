b2cce03e8411a7116397f83f89e4bd22e4137920
nahowo <nahowo@naver.com>
2023-11-24 11:37:20 +0900

    delete vote

--- Orb/settings.py
+++ Orb/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vote',
-    'rest_framework',
+    'rest_frameword',
     'wordcloud',
 ]
 
--- vote/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from .views import TeamViewSet, VoteView
-
-urlpatterns = [
-    path('', TeamViewSet.as_view({'get': 'list'})),
-    path('<int:pk>/', TeamViewSet.as_view({'get': 'retrieve'})),
-    path('<int:team_id>/', VoteView.as_view()),
-]
