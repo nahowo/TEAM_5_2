7631e7353b20f18ac9061dc4be12d73ea1684a14
bamcasa <combeesang@gmail.com>
2023-11-25 08:40:54 +0900

    Feat: 설명 공백, null 가능

--- recommend/models.py
+++ recommend/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=20)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     voted_count = models.IntegerField(default=0)
 
