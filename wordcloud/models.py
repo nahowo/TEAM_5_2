cb33e17809702510add7fcc964d6913519d458ed
bamcasa <combeesang@gmail.com>
2023-11-25 09:30:45 +0900

    Feat: 좋아요수 기본값 0

--- wordcloud/models.py
+++ wordcloud/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Wordcloud(models.Model):
     word = models.CharField(max_length=100)
-    likes = models.IntegerField()
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.word
